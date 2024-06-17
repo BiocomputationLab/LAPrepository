50405ed40ef36ba9e569fcac8efc6a1bd0c3f8b3
Ana-Mariya Anhel <67283883+Ana-MAV@users.noreply.github.com>
2024-06-17 15:49:18 +0200

    Add files via upload
    A floating point error mitigation is introduced by given some margin error when comparing the needed volume and the one given in the tube
    If not even 1 reaction can be substracted from the volume without changing the height of aspiration/dispense, 0 is returned when previously 1 was returned

--- SetFunctions/calculate_max_reactions_constant_height_15mLfalcon.py
+++ SetFunctions/calculate_max_reactions_constant_height_15mLfalcon.py
@@ -7,13 +7,17 @@ def calculate_max_reactions_constant_height_15mLfalcon (tube, vol_tube, total_nu
 	"""
 
 	# Check if there is enough volume in the tube to transfer all the reactions
-	if vol_tube < total_number_reactions*vol_per_reaction:
+	if vol_tube - (total_number_reactions*vol_per_reaction) < -0.001:
 		raise Exception(f"Not enough volume in the source tube, {vol_tube}uL, to distribute {vol_per_reaction}uL to {total_number_reactions} reactions")
 	
 	react_distr = 0
-	
+
+	# Let's see if at least there is 1*volume reaction can be transferred without changing
+	if find_safe_15mLfalcon_height(vol_tube, tube).point != find_safe_15mLfalcon_height(vol_tube - vol_per_reaction, tube).point:
+		return 0 # This will mean that no volume of reaction can be moved without changing the volume so it needs another way to deal with it
+
 	# Loop adding 1 reaction until the height of aspirate change
-	while find_safe_15mLfalcon_height (vol_tube, tube).point == find_safe_15mLfalcon_height (vol_tube-(react_distr*vol_per_reaction), tube).point:
+	while find_safe_15mLfalcon_height(vol_tube, tube).point == find_safe_15mLfalcon_height(vol_tube - (react_distr*vol_per_reaction), tube).point:
 		if react_distr + 1 > total_number_reactions:
 			break
 		else: # One more reaction can be transfered
